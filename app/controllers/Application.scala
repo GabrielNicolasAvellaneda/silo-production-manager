f73cff8881c395d17188eb8e55eacab0154010dc
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-10-04 23:24:43 -0300

    Added initial layout.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,12 @@
 package controllers
 
-import play.api._
+import model.{Product, Db}
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
 }
